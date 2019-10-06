6203b643295f50a1ae22b31a669a20b28f64ade0
Terry Fadz <terry@glantz.net>
2019-10-06 13:30:41 -0500

    fixed untracked files

--- app/js/theme/mobile-nav.js
+++ app/js/theme/mobile-nav.js
@@ -3,6 +3,7 @@ jQuery(function($) {
 	$('.elm-nav-mobile__pull').click(function(event) {
 		$('.elm-nav-mobile__links').toggleClass('elm-nav-mobile__links__show');
 		$('.elm-nav-mobile__mask').toggleClass('elm-nav-mobile__mask--show');
+    $('.elm-nav-mobile__links__list').addClass('show');
 
 	});
 });
